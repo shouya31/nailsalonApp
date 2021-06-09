6ed2412b4651951916821056c5a19e33565c298f
higuchi shouya <higuchishouya@MacBook-Pro-2.local>
2021-06-09 17:27:38 +0900

    フォームテスト

--- js/form.js
+++ js/form.js
@@ -1,3 +1,29 @@
-// ①送信のイベント発火から、フォームの値を取得する
+// // ①送信のイベント発火から、フォームの値を取得する
 
-// ②取得した値を指定のパスにPOSTで送信する
+// // ②取得した値を指定のパスにPOSTで送信する
+// window.addEventListener("DOMContentLoaded", function() {
+//     document.getElementById('form').addEventListener('submit', function(e){
+//         e.preventDefault()
+
+//         //　FormDataオブジェクトを用いて、フォームの値を取得する
+//         const form = new FormData(this)
+
+//         // GoogleAPIを用いて、スプレッドシートに書き込み作業を行う
+
+
+//     })
+//  });
+
+document.querySelector("form").addEventListener("submit", handleSubmit);
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  let myForm = document.getElementById('pizzaOrder');
+  let formData = new FormData(myForm)
+  fetch('/', {
+    method: 'POST',
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString()
+  }).then(() => console.log('Form successfully submitted')).catch((error) =>
+    alert(error))
+}
