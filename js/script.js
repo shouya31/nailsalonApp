9d2ec8bf7dd302b15aa8b02e575e8a8b09ef6fff
higuchi shouya <higuchishouya@MacBook-Pro-2.local>
2021-07-08 22:47:56 +0900

    itemsページのメニュー不具合修正

--- js/script.js
+++ js/script.js
@@ -1,25 +1,24 @@
-if (window.matchMedia('(min-width: 959px)').matches) {
-    $(window).on('load scroll', function(){
-        if ($(window).scrollTop() > 200) {
-          $('.header_wrapper').fadeIn(400);
-         } else {
-          $('.header_wrapper').fadeOut(400);
-         }
-      });
+if (location.href === location.origin + "/") {
+  if (window.matchMedia("(min-width: 959px)").matches) {
+    $(window).on("load scroll", function () {
+      if ($(window).scrollTop() > 200) {
+        $(".header_wrapper").fadeIn(400);
+      } else {
+        $(".header_wrapper").fadeOut(400);
+      }
+    });
   } else {
     $(function () {
-        $('.zdo_drawer_button').click(function () {
-          $(this).toggleClass('active');
-          $('.header_wrapper').fadeToggle();
-        })
-      })
-      $(function () {
-        $('.header_nav,.header_cta-btn a').click(function () {
-          $('.header_wrapper').fadeOut();
-          $('.zdo_drawer_button').toggleClass('active');
-        })
-      })
+      $(".zdo_drawer_button").click(function () {
+        $(this).toggleClass("active");
+        $(".header_wrapper").fadeToggle();
+      });
+    });
+    $(function () {
+      $(".header_nav,.header_cta-btn a").click(function () {
+        $(".header_wrapper").fadeOut();
+        $(".zdo_drawer_button").toggleClass("active");
+      });
+    });
   }
-
-
-  
+}
