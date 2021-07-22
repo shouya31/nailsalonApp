d1a47f14c4ac1b2b3240345b7430318df9a112d0
higuchi shouya <higuchishouya@MacBook-Pro-2.local>
2021-07-22 19:38:18 +0900

    文字切れ改修

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,12 @@
 $(function () {
-    $('.zdo_drawer_button').click(function () {
-      $(this).toggleClass('active');
-      $('.header_wrapper').fadeToggle();
-    })
-  })
-  $(function () {
-    $('.header_nav,.header_cta-btn a').click(function () {
-      $('.header_wrapper').fadeOut();
-      $('.zdo_drawer_button').toggleClass('active');
-    })
-  })
+  $(".zdo_drawer_button").click(function () {
+    $(this).toggleClass("active");
+    $(".header_wrapper").fadeToggle();
+  });
+});
+$(function () {
+  $(".header_nav,.header_cta-btn a").click(function () {
+    $(".header_wrapper").fadeOut();
+    $(".zdo_drawer_button").toggleClass("active");
+  });
+});
